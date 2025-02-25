a7900d7c1fc63d77dbc1050d8c2b827bfec8ba15
zae-vanza <zaevanza.rpl25@hyperdata.biz>
2025-02-25 16:56:20 +0700

    hapus template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,24 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function __construct()
-    {
-        $this->arrbc = [
-            [
-                'Master',
-                'Warehouse Locator'
-            ]
-            ];
-    }
-
     public function index()
     {
-        $data = [
-            'title' => 'Warehouse Locator',
-            'breadcrumb' => $this->arrbc,
-            'section' => 'Warehouse Locator',
-            'akses' => 'Administrator',
-        ];
-        return view('master/city/v_city', $data);
+        return view('welcome_message');
     }
 }
--- app/Views/master/city/v_city.php
+++
@@ -1,2 +0,0 @@
-<?= $this->include('template/v_appbar.php') ?>
-<?= $this->include('template/v_header.php') ?>
