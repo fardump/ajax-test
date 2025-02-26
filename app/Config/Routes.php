647344dd0c7d58f7cf1027264389ea50ffc75ddd
Alfaluiszzz11 <bintang.rpl25@hyperdata.biz>
2025-02-26 10:24:36 +0700

    form user

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,21 +7,23 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'User::index');
 
-$routes->group('province', function($routes){
+$routes->group('province', function ($routes) {
     $routes->get('', 'Province::index');
 });
-$routes->group('user', function($routes){
+$routes->group('user', function ($routes) {
     $routes->get('', 'User::index');
+    $routes->add('deleteUsers', 'User::deleteUsers');
+    $routes->add('add', 'User::add');
 });
-$routes->group('category', function($routes){
+$routes->group('category', function ($routes) {
     $routes->get('', 'Category::index');
 });
-$routes->group('type', function($routes){
+$routes->group('type', function ($routes) {
     $routes->get('', 'Type::index');
 });
-$routes->group('city', function($routes){
+$routes->group('city', function ($routes) {
     $routes->get('', 'City::index');
 });
-$routes->group('ekspedition', function($routes){
+$routes->group('ekspedition', function ($routes) {
     $routes->get('', 'Ekspedition::index');
 });
